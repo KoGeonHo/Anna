2ca0e70f48f6e4983c4db62983080486f79d6056
newael <ksm980314@naver.com>
2022-07-12 09:10:58 +0900

    22-07-12
    사진 경로&이름이 db에 안 올라감

---
+++ anna/src/main/java/edu/fourmen/controller/BoardItemController.java
@@ -0,0 +1,158 @@
+package edu.fourmen.controller;
+
+import java.io.File;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import edu.fourmen.dao.BoardItemDAO;
+import edu.fourmen.service.BoardItemService;
+import edu.fourmen.vo.BoardItemVO;
+import edu.fourmen.vo.UserVO;
+
+@RequestMapping(value="/boarditem")
+@Controller
+public class BoardItemController {
+	
+	@Autowired
+	BoardItemService boarditemService;
+	
+	@RequestMapping(value="itemlist.do")
+	public String Trade(BoardItemVO vo, Model model) {
+		
+		List<BoardItemVO> list = boarditemService.list(vo);
+		model.addAttribute("list", list);
+		
+		return "boarditem/itemlist";
+	}
+
+	
+	@RequestMapping(value="itemview.do")
+	public String selectitem(int item_idx,HttpServletResponse response, HttpServletRequest request, HttpSession session, Model model) {
+		session = request.getSession();
+		UserVO login = (UserVO)session.getAttribute("login");
+		model.addAttribute("login",login);
+		BoardItemVO vo = boarditemService.selectitem(item_idx);
+		model.addAttribute("vo",vo);
+		return "boarditem/itemview";
+		
+	}
+	
+	@RequestMapping(value="itemwrite.do", method=RequestMethod.GET)
+	public String itemwrite() {
+		
+		return "boarditem/itemwrite";
+	}
+	
+	@RequestMapping(value="itemwrite.do", method=RequestMethod.POST)
+	public String itemwrite(BoardItemVO vo,Map<String, Object> map,MultipartFile file, HttpServletRequest request,HttpServletResponse response,  HttpSession session, MultipartHttpServletRequest mull,Model model) throws IllegalStateException, IOException {
+		
+	
+		String path = request.getSession().getServletContext().getRealPath("/resources/upload");
+		System.out.println(path);
+		
+		/*
+		 * if(!file.getOriginalFilename().isEmpty()) {//화면에서 넘어온 파일이 존재한다면
+		 * file.transferTo(new File(path, file.getOriginalFilename())); //화면에서 넘어온 파일을
+		 * //path 위치에 새로쓰는 로직
+		 * 
+		 * }else { System.out.println("업로드할 파일이 존재하지 않습니다."); }
+		 */
+		
+		File dir = new File(path); // path가 존재하는지 여부 확인
+		
+		
+		if(!dir.exists()) {
+			dir.mkdirs(); // 위치가 존재하지 않는 경우 위치 생성
+		}
+		
+		
+		session = request.getSession();
+		
+		UserVO login = (UserVO)session.getAttribute("login");
+		
+		//vo.setMidx(login.getMidx());
+		
+		int result = boarditemService.boarditemswrite(vo,map,request);
+		
+		model.addAttribute("vo",vo);
+		
+		response.setContentType("text/html;charset=utf-8");
+		
+		
+		
+		
+		/*
+		 * PrintWriter pw = response.getWriter();
+		 * 
+		 * if(result <= 0) {
+		 * pw.append("<script>alert('글쓰기 실패.');location.href='list.do';</script>");
+		 * 
+		 * pw.flush();
+		 * 
+		 * }else {
+		 * pw.append("<script>alert('글쓰기 성공.');location.href='itemview.do?bidx="+vo.
+		 * getBidx()+"';</script>"); pw.flush(); }
+		 */
+		
+		return "redirect:/boarditem/itemview.do?item_idx="+vo.getItem_idx();
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ anna/src/main/java/edu/fourmen/vo/BoardItemVO.java
@@ -0,0 +1,189 @@
+package edu.fourmen.vo;
+
+public class BoardItemVO extends UserVO {
+	
+	private int item_idx;
+	private int uidx;
+	private int cate_idx;
+	private String Title;
+	private String Content;
+	private String nickname;
+	private String wdate;
+	private String addr1;
+	private String addr2;
+	private String addr_Code;
+	private String image1;
+	private String image2;
+	private String image3;
+	private String image4;
+	private String image5;
+	private String image6;
+	private String image7;
+	private String image8;
+	private String image9;
+	private String image10;
+	private String Keyword;
+	private String Price;
+	private String offer;
+	private int hit;
+	private int state;
+	
+	
+	
+	
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	public int getItem_idx() {
+		return item_idx;
+	}
+	public void setItem_idx(int item_idx) {
+		this.item_idx = item_idx;
+	}
+	public int getUidx() {
+		return uidx;
+	}
+	public void setUidx(int uidx) {
+		this.uidx = uidx;
+	}
+	public int getCate_idx() {
+		return cate_idx;
+	}
+	public void setCate_idx(int cate_idx) {
+		this.cate_idx = cate_idx;
+	}
+	public String getTitle() {
+		return Title;
+	}
+	public void setTitle(String title) {
+		Title = title;
+	}
+	public String getContent() {
+		return Content;
+	}
+	public void setContent(String content) {
+		Content = content;
+	}
+	public String getWdate() {
+		return wdate;
+	}
+	public void setWdate(String wdate) {
+		this.wdate = wdate;
+	}
+	public String getAddr1() {
+		return addr1;
+	}
+	public void setAddr1(String addr1) {
+		this.addr1 = addr1;
+	}
+	public String getAddr2() {
+		return addr2;
+	}
+	public void setAddr2(String addr2) {
+		this.addr2 = addr2;
+	}
+	public String getAddr_Code() {
+		return addr_Code;
+	}
+	public void setAddr_Code(String addr_Code) {
+		this.addr_Code = addr_Code;
+	}
+	public String getImage1() {
+		return image1;
+	}
+	public void setImage1(String image1) {
+		this.image1 = image1;
+	}
+	public String getImage2() {
+		return image2;
+	}
+	public void setImage2(String image2) {
+		this.image2 = image2;
+	}
+	public String getImage3() {
+		return image3;
+	}
+	public void setImage3(String image3) {
+		this.image3 = image3;
+	}
+	public String getImage4() {
+		return image4;
+	}
+	public void setImage4(String image4) {
+		this.image4 = image4;
+	}
+	public String getImage5() {
+		return image5;
+	}
+	public void setImage5(String image5) {
+		this.image5 = image5;
+	}
+	public String getImage6() {
+		return image6;
+	}
+	public void setImage6(String image6) {
+		this.image6 = image6;
+	}
+	public String getImage7() {
+		return image7;
+	}
+	public void setImage7(String image7) {
+		this.image7 = image7;
+	}
+	public String getImage8() {
+		return image8;
+	}
+	public void setImage8(String image8) {
+		this.image8 = image8;
+	}
+	public String getImage9() {
+		return image9;
+	}
+	public void setImage9(String image9) {
+		this.image9 = image9;
+	}
+	public String getImage10() {
+		return image10;
+	}
+	public void setImage10(String image10) {
+		this.image10 = image10;
+	}
+	public String getKeyword() {
+		return Keyword;
+	}
+	public void setKeyword(String keyword) {
+		Keyword = keyword;
+	}
+	public String getPrice() {
+		return Price;
+	}
+	public void setPrice(String price) {
+		Price = price;
+	}
+	public String getOffer() {
+		return offer;
+	}
+	public void setOffer(String offer) {
+		this.offer = offer;
+	}
+	public int getHit() {
+		return hit;
+	}
+	public void setHit(int hit) {
+		this.hit = hit;
+	}
+	public int getState() {
+		return state;
+	}
+	public void setState(int state) {
+		this.state = state;
+	}
+	
+	
+	
+	
+	
+}
