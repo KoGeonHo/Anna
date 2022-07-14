11d1e0888a4eed6491b27cf790d5eeb95b56c613
GeonHo Ko <92087568+KoGeonHo@users.noreply.github.com>
2022-07-14 12:09:24 +0900

    수정 2022.07.14
    test

--- anna/src/main/java/edu/fourmen/controller/UserController.java
+++ anna/src/main/java/edu/fourmen/controller/UserController.java
@@ -60,7 +60,7 @@ public class UserController {
 			
 			System.out.println("가입되지 않은 이메일주소");
 			
-			pw.append("<script>alert('환영합니다"+vo.getNickName()+"님, 가입이 완료되었습니다.'); location.href='"+request.getContextPath()+"/user/login.do';</script>");
+			pw.append("<script>alert('가입되지 않은 이메일 주소입니다.'); history.back();</script>");
 			
 			pw.flush();
 			
