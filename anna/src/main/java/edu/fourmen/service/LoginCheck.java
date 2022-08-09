21be28ce47cce85ec2dc35e3e25ff42c5c79be1d
GeonHo Ko <92087568+KoGeonHo@users.noreply.github.com>
2022-08-09 11:01:45 +0900

    Revert "20220809"
    This reverts commit f18a5d414be19002b3002709d772c6522dec1bb1.

--- anna/src/main/java/edu/fourmen/service/LoginCheck.java
+++ anna/src/main/java/edu/fourmen/service/LoginCheck.java
@@ -26,11 +26,8 @@ public class LoginCheck extends HandlerInterceptorAdapter{
 			if(cookie.getName().equals("uidx")) {
 <<<<<<< HEAD
 <<<<<<< HEAD
-<<<<<<< HEAD
 				System.out.println("쿠키존재:"+cookie.getValue());
 =======
-=======
->>>>>>> parent of 89a0040 (Revert "Revert "20220809"")
 =======
 >>>>>>> parent of 89a0040 (Revert "Revert "20220809"")
 				System.out.println(cookie.getValue()); 
@@ -46,9 +43,6 @@ public class LoginCheck extends HandlerInterceptorAdapter{
 				
 				return true;
 <<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> parent of 89a0040 (Revert "Revert "20220809"")
-=======
 >>>>>>> parent of 89a0040 (Revert "Revert "20220809"")
 =======
 >>>>>>> parent of 89a0040 (Revert "Revert "20220809"")
