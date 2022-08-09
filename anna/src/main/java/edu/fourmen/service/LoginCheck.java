2946d0584804d99661fbaba4b17f11896d344089
GeonHo Ko <92087568+KoGeonHo@users.noreply.github.com>
2022-08-09 11:01:06 +0900

    20220809
    푸시 후 수정

--- anna/src/main/java/edu/fourmen/service/LoginCheck.java
+++ anna/src/main/java/edu/fourmen/service/LoginCheck.java
@@ -25,8 +25,11 @@ public class LoginCheck extends HandlerInterceptorAdapter{
 		for(Cookie cookie:cookies) {
 			if(cookie.getName().equals("uidx")) {
 <<<<<<< HEAD
+<<<<<<< HEAD
 				System.out.println("쿠키존재:"+cookie.getValue());
 =======
+=======
+>>>>>>> parent of 89a0040 (Revert "Revert "20220809"")
 				System.out.println(cookie.getValue()); 
 				//System.out.println("cookie:"+Integer.parseInt(String.valueOf(cookieUidx)));
 				//System.out.println("쿠키존재:"+cookie.getValue());
@@ -39,6 +42,9 @@ public class LoginCheck extends HandlerInterceptorAdapter{
 				
 				
 				return true;
+<<<<<<< HEAD
+>>>>>>> parent of 89a0040 (Revert "Revert "20220809"")
+=======
 >>>>>>> parent of 89a0040 (Revert "Revert "20220809"")
 			}
 		}
